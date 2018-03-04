0c3b73eaba8f1c0dc2e02ab0a143a5f74f38c131
trevortds <trevortds3@gmail.com>
2018-03-04 14:40:55 -0500

    New mainactivity with androidstudio defaults.

--- app/src/main/java/com/etymachine/commutemodel/MainActivityOld.java
+++ app/src/main/java/com/etymachine/commutemodel/MainActivityOld.java
@@ -1,10 +1,7 @@
 package com.etymachine.commutemodel;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -15,24 +12,13 @@ import com.etymachine.commutemodel.db.TimeEntry;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityOld extends AppCompatActivity {
     private List<TimeEntry> returnedResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_main_old);
     }
 
     public void populateTestData(View view) {
